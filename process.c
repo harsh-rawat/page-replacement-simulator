02e241735193375189349b84d6c8ba3d5d72f1c0
Harsh Rawat <harsh.rawat@wisc.edu>
2020-11-28 19:17:51 -0600

    Partial parser for trace file. Integration with hashtable required.

--- process.c
+++ process.c
@@ -1,2 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "process.h"
 
+void* ParseTraceFile(char* filepath){
+    FILE* file = fopen(filepath, "r");
+    int lineIndex = 0;
+
+    while (feof(file) == 0) {
+        memory_reference *reference = ReadLine(file, lineIndex);
+        //Check if the process id exists in the hashtable
+        process *existing_process = NULL;//Get the process from hashtable
+        if(existing_process == NULL){//If process does not exist in hashtable
+            process *new_process = malloc(sizeof(process));
+            new_process->pid = reference->pid;
+            new_process->start = lineIndex;
+            new_process->end = lineIndex;
+            //Put this new process in hashtable
+        } else {//Take the existing process object and update the last value
+            existing_process->end = lineIndex;
+        }
+        free(reference);
+        lineIndex++;
+    }
+    //return hashtable reference
+}
--- process.h
+++ process.h
@@ -1,11 +1,16 @@
 #ifndef ASSIGNMENT4_PROCESS_H
 #define ASSIGNMENT4_PROCESS_H
 
+#include "input_parser.h"
+#include "error_handler.h"
+
 struct process {
     int pid;
     int start;
     int end;
 };
 typedef struct process process;
+
+void* ParseTraceFile(char* filepath);
 
 #endif
